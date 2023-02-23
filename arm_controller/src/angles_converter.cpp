027fbd37b45a348a335ea048d2e34ff85db39caf
Vitalii Pepko <vitaliipepko@gmail.com>
2023-02-23 20:41:10 +0200

    add angles converter

---
+++ arm_controller/src/angles_converter.cpp
@@ -0,0 +1,10 @@
+#include "ros/ros.h"
+#include "arm_controller/AnglesConverter.h"
+#include "math.h"
+
+
+int main(int argc, char * argv[])
+{
+    ros::init(argc, argv, "angles_converter");
+    return 0;
+}
